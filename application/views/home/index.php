5db26a4013655349e4b495ca30ccc0f4c5d59774
ulza123 <ulza.alkindi@students.amikom.ac.id>
2018-12-28 18:41:00 +0700

    Import CSC include

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,5 +1,5 @@
 <div class="container">
-	<div class="jumbotron">
+	<div class="jumbotron mt-4">
 	  <h1 class="display-4">Hello,Selamat Datang Di Website Kami</h1>
 	  <p class="lead">Ini Adalah website CRUD untuk Responsi Pemrograman Web </p>
 	  <hr class="my-4">
